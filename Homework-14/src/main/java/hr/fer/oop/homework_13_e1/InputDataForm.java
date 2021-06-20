b14745f0c9527764a92c9fb9590fe20ffe52abfd
filipfabris1 <86193302+filipfabris1@users.noreply.github.com>
2021-06-20 16:24:16 +0200

    Add files via upload

---
+++ Homework-14/src/main/java/hr/fer/oop/homework_13_e1/InputDataForm.java
@@ -0,0 +1,28 @@
+package hr.fer.oop.homework_13_e1;
+
+import java.awt.GridLayout;
+
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+public class InputDataForm extends JPanel {
+	JTextField txName = new JTextField();
+	JCheckBox chVaccinated = new JCheckBox("vaccinated");
+	JTextField txAddress = new JTextField();
+
+	public InputDataForm() {
+		setLayout(new GridLayout(3, 2, 5, 5));
+
+		add(new JLabel("Name:", SwingConstants.RIGHT));
+		add(txName);
+
+		add(new JLabel()); // placeholder
+		add(chVaccinated);
+
+		add(new JLabel("Address:", SwingConstants.RIGHT));
+		add(txAddress);
+	}
+}
